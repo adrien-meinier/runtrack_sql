5ce1163894ece2bdbe292b02a7d95a045f938654
adrien meinier <adrien.meinier@laplateforme.io>
2026-03-03 11:45:50 +0100

    la suite

---
+++ jour2/job6.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+try:
+    connexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="noein210494",
+        database="LaPlateforme"
+    )
+
+    curseur = connexion.cursor()
+
+    requete = "SELECT SUM(capacite) FROM salle"
+    curseur.execute(requete)
+
+    resultat = curseur.fetchone()
+    capacite_totale = resultat[0]
+
+    print(f"La capacité totale des salles est de {capacite_totale} personnes")
+
+except mysql.connector.Error as erreur:
+    print("Erreur :", erreur)
+
+finally:
+    if connexion.is_connected():
+        curseur.close()
+        connexion.close()
